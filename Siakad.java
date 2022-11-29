6c7dfe3cd6fcc0800a8190b11ea2e8d75d7cbc1c
unknown <nadiaputriramadhoni01@gmail.com>
2022-11-29 16:01:59 +0700

    commit minggu keempatbelas

--- Siakad.java
+++ Siakad.java
@@ -25,8 +25,35 @@ public class Siakad {
         System.out.println("Berikut data Anda");
         int i = 0;
         while (i<jumlahData) {
-            System.out.println(mahasiswa[i].getNama() + "" + mahasiswa[i].getNim());
+            System.out.println(mahasiswa[i].getNama() + " " + mahasiswa[i].getNim());
             i++;
+        }
+    }
+
+    public static void urutData(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Pilih algoritma pengurutan");
+        System.out.println("1. EXCHANGE SORT");
+        System.out.println("Pilih algoritma = ");
+        int algo =  scan.nextInt();
+        switch (algo) {
+            case 1: {
+                exchangeSort();
+                break;
+            }
+        }
+
+    }
+
+    public static void exchangeSort() {
+        for (int x = 0; x<jumlahData; x++) {
+            for (int y = x+1; y<jumlahData; y++) {
+                if (mahasiswa[x].getNim().compareTo(mahasiswa[y].getNim())>=1) {
+                    Mahasiswa temp = mahasiswa[x];
+                    mahasiswa[x] = mahasiswa[y];
+                    mahasiswa[y] = temp;
+                }
+            }
         }
     }
 
@@ -37,16 +64,20 @@ public class Siakad {
             System.out.println("Menu Siakad");
             System.out.println("1. Tambah Data");
             System.out.println("2. Tampilkan Data");
-            System.out.println("3. Keluar");
+            System.out.println("3. Urutkan Data");
+            System.out.println("4. Keluar");
             System.out.print("Pilih Menu = ");
             menu = scan.nextInt();
-            if (menu ==1){
+            if (menu ==1) {
                 tambahData();
                 //System.out.println("Silakan tambah data");
             } else if (menu==2) {
                 lihatData();
                 //System.out.println("Berikut data Anda");
+            } else if (menu==3){
+                urutData();
+                //System.out.println("Data Anda sudah diurutkan. Berikut data Anda:");
             }
-        } while (menu!= 3);
+        } while (menu!= 4);
     }
 }
