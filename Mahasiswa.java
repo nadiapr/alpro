341bab727523861cf06b5e369d383e0286456e08
unknown <nadiaputriramadhoni01@gmail.com>
2022-12-31 11:52:03 +0700

    uas alproo

---
+++ Mahasiswa.java
@@ -0,0 +1,20 @@
+public class Mahasiswa {
+    private int nim;
+    private String nama;
+
+    public void setNim(int nim) {
+        this.nim = nim;
+    }
+
+    public int getNim() {
+        return this.nim;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return this.nama;
+    }
+}
